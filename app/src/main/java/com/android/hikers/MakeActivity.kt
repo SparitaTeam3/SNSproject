90a817049e24015ee493ec3ab203de80878dd2d3
이성재 <sj5lee1@gmail.com>
2023-12-27 12:59:23 +0900

    엑티비티 이름을 main에서 make으로 바꿈

--- app/src/main/java/com/android/hikers/MakeActivity.kt
+++ app/src/main/java/com/android/hikers/MakeActivity.kt
@@ -3,7 +3,7 @@ package com.android.hikers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MakeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
