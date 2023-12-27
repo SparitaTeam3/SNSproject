447277412a71de1d61af0f85b843e0d946bf473e
이성재 <sj5lee1@gmail.com>
2023-12-27 14:19:32 +0900

    파일 이름을 좀 더 명확하게 바꿈

--- app/src/main/java/com/android/hikers/MakeNewPostActivity.kt
+++ app/src/main/java/com/android/hikers/MakeNewPostActivity.kt
@@ -3,9 +3,9 @@ package com.android.hikers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MakeActivity : AppCompatActivity() {
+class MakeNewPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_makenewpost)
     }
 }
