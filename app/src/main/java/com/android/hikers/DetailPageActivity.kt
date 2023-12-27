fee8c9f976ebdb4328cd6dc7d564b62704bd32fc
이성재 <sj5lee1@gmail.com>
2023-12-27 20:48:29 +0900

    merge에 맞게 고쳐봄

--- app/src/main/java/com/android/hikers/DetailPageActivity.kt
+++ app/src/main/java/com/android/hikers/DetailPageActivity.kt
@@ -2,10 +2,12 @@ package com.android.hikers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class DetailPageActivity : AppCompatActivity() {
+class UserInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailpage)
+
     }
 }
