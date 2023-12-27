bdd4a315362a1ecaff955f9d42de4475875d51fd
이성재 <sj5lee1@gmail.com>
2023-12-27 21:50:12 +0900

    merge conflict 때문에 branch 다시 만들고 완성한 레이아웃 추가

--- app/src/main/java/com/android/hikers/DetailPageActivity.kt
+++ app/src/main/java/com/android/hikers/DetailPageActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class UserInfoActivity : AppCompatActivity() {
+class DetailPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailpage)
