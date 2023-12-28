43656f993de26d86e74c9c5cf10c1da1398bde09
parkjaewon <wodnjs3336@naver.com>
2023-12-28 16:56:40 +0900

    게시물 출력, 스크롤 레이아웃 수정

--- app/src/main/java/com/android/hikers/MyPage.kt
+++ app/src/main/java/com/android/hikers/MyPage.kt
@@ -1,8 +1,10 @@
 package com.android.hikers
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -11,6 +13,8 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.android.hikers.data.PostManager
 import com.android.hikers.data.UserManager
 
 
@@ -20,26 +24,39 @@ class MyPage : AppCompatActivity() {
     private val name: TextView by lazy { findViewById(R.id.tv_name) }
     private val id: TextView by lazy { findViewById(R.id.tv_id) }
     private val introduction: TextView by lazy { findViewById(R.id.tv_myinfo) }
+    private val noPost: TextView by lazy { findViewById(R.id.tv_no_recent) }
     private val profilePhoto: ImageView by lazy { findViewById(R.id.img_profile) }
     private val back: ImageButton by lazy { findViewById(R.id.btn_back) }
     private val setting: Button by lazy { findViewById(R.id.btn_edit) }
-    private val userManager = UserManager.newInstance()
     private val myPageScrollView: HorizontalScrollView by lazy { findViewById(R.id.scrollview) }
     private val myPageScrollView2: HorizontalScrollView by lazy { findViewById(R.id.likescrollview) }
 
+    private val userManager = UserManager.newInstance()
+    private val postManager = PostManager.newInstance()
+
     //현재 로그인 한 유저의 아이디
     private val userID by lazy {
-        "hong_gildong"
+        "lee_younghee"
         //intent.getStringExtra("userID") ?: ""
     }
 
     private val postItemList by lazy {
         listOf<ViewGroup>(
-            findViewById(R.id.post_item1),
-            findViewById(R.id.post_item2),
-            findViewById(R.id.post_item3),
-            findViewById(R.id.post_item4),
-            findViewById(R.id.post_item5)
+            findViewById(R.id.mypage_post1),
+            findViewById(R.id.mypage_post2),
+            findViewById(R.id.mypage_post3),
+            findViewById(R.id.mypage_post4),
+            findViewById(R.id.mypage_post5)
+        )
+    }
+
+    private val postItemIDMap by lazy {
+        mutableMapOf<Int, Int>(
+            R.id.mypage_post1 to -1,
+            R.id.mypage_post2 to -1,
+            R.id.mypage_post3 to -1,
+            R.id.mypage_post4 to -1,
+            R.id.mypage_post5 to -1
         )
     }
 
@@ -49,11 +66,13 @@ class MyPage : AppCompatActivity() {
 
         setViewMore(info, more)
 
+        initProfile()
+
+        initHorizontalScrollView()
+
         goToMain()
 
         goToUserInfo()
-
-        initProfile2()
 
     }
 
@@ -71,12 +90,14 @@ class MyPage : AppCompatActivity() {
                         viewMoreTextView.visibility = View.GONE
                         info.setMovementMethod(ScrollingMovementMethod())
                     }
+                } else {
+                    viewMoreTextView.visibility = View.GONE
                 }
             }
         }
     }
 
-    private fun initProfile2() {
+    private fun initProfile() {
         val loginUser = userManager.findUserByID(userID)!!
         val userName = loginUser.name
         val userImage = loginUser.profileImage
@@ -85,10 +106,48 @@ class MyPage : AppCompatActivity() {
 
         name.text = userName
         id.text = userId
+        //아직 소개가 없어서 주석 처리 함
         //introduction.text = userintroduction
         profilePhoto.run {
             if (userImage != null) setImageURI(userImage)
             else setImageResource(R.drawable.default_profile)
+        }
+    }
+
+    private fun initHorizontalScrollView() {
+        postItemList.forEach { it.isVisible = false }
+        val recentPostList = postManager.getRecentPostList(3)
+
+        if (recentPostList.isEmpty()) {
+            noPost.isVisible = true
+            myPageScrollView.isVisible = false
+            return
+        }
+
+        noPost.isVisible = false
+        myPageScrollView.isVisible = true
+        for ((index, post) in recentPostList.withIndex()) {
+            val postItem = postItemList[index]
+
+            postItemIDMap[postItem.id] = post.postID
+            postItem.isVisible = true
+
+            val imageView = postItem.findViewById<ImageView>(R.id.pt_image)
+            val titleText = postItem.findViewById<TextView>(R.id.tv_post_title)
+            val nameText = postItem.findViewById<TextView>(R.id.tv_post_name)
+
+            //아직 사진이 없어서 주석 처리 함
+//            imageView.run {
+//                if (post.image != null) {
+//                    setImageURI(post.image)
+//                    scaleType = ImageView.ScaleType.CENTER_CROP
+//                } else {
+//                    setImageResource(R.drawable.hikers_icon_small_grey)
+//                    scaleType = ImageView.ScaleType.CENTER
+//                }
+//            }
+            titleText.text = post.title
+            nameText.text = userManager.findUserByID(post.writerId)!!.name
         }
     }
 
@@ -102,8 +161,11 @@ class MyPage : AppCompatActivity() {
 
     private fun goToUserInfo() {
         setting.setOnClickListener {
-            val intent = Intent(this, UserInfoActivity::class.java)
-            startActivity(intent)
+            Log.d(TAG, "edit info button clicked")
+            val editIntent = Intent(this, UserInfoActivity::class.java).apply {
+                putExtra("userID", userID)
+            }
+            startActivity(editIntent)
         }
     }
 }
